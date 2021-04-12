77eee2b6ba745e38ff78d14455ad5f4b3780d72e
탁균 이 <vbmrkk@naver.com>
2021-04-12 16:24:59 +0900

    MemberList에 add enum ClassIndex

--- WindowsFormsApp1/WindowsFormsApp1/MEMBER/MemberList.cs
+++ WindowsFormsApp1/WindowsFormsApp1/MEMBER/MemberList.cs
@@ -8,6 +8,16 @@ namespace WindowsFormsApp1.MEMBER
 {
     class MemberList : List<IMember>, IMember
     {
+        public enum ClassIndex : short
+        {
+            BOOK_MANAGER =0,
+            BOOK_SEARTCH,
+            BOOK_RENT,
+            READDING_MANAGE,
+            READDING_RENT,
+            MEETTING_MANAGE,
+            MEETTING_RENT,
+        };
         public void Logout()
         {
             foreach (var item in this)
