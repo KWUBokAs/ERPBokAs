c42db0a673a1b7828a37b5a32960271d23f6e958
EricSamuelJun <mam675@naver.com>
2021-05-06 03:38:56 +0900

    image 추가 및 로딩창 추가

---
+++ WindowsFormsApp1/WindowsFormsApp1/BACK/LoadingForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.BACK {
+    public partial class LoadingForm : Form {
+        private Image gifimage;
+        public LoadingForm() {
+            InitializeComponent();
+            //gifimage = Image.FromFile("Resources://loading.gif");
+        }
+    }
+}
