a8bebb613cf604386d38e0e3df0ad963a54c8d5e
EricSamuelJun <mam675@naver.com>
2021-05-05 21:16:14 +0900

    form 2 만듬 이거 보고 샘플 하셈

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+    public partial class Form2 : Form {
+        public const int panel2locdef = 232;
+        public const int panel1heightdef = 183;
+        public const int panel1changesize = 70;
+        public Form2() {
+            InitializeComponent();
+            this.Load += UserControlShow;
+        }
+        private void UserControlShow(Object Sender, EventArgs e) {
+            UserControl user = new UserControl1();
+            panel1.Controls.Add(user);
+        }
+        public void setSizeLittle() {
+            Console.WriteLine("set Size small");
+            panel1.Height -= panel1changesize;
+            panel2.Top = panel2locdef - panel1changesize;
+        }
+        public void setSizeBig() {
+            Console.WriteLine("set Size Big");
+            panel1.Height += panel1changesize;
+            panel2.Top = panel2locdef;
+        }
+    }
+}
