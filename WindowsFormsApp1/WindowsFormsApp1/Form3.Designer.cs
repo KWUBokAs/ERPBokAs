efb679835b08b6667c939d90077222996a7791ce
탁균 이 <vbmrkk@naver.com>
2021-06-07 04:07:34 +0900

    한번더 회원정보 패널 수정

--- WindowsFormsApp1/WindowsFormsApp1/Form3.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form3.Designer.cs
@@ -39,15 +39,15 @@ namespace WindowsFormsApp1
             this.labTime = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel3.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -68,7 +68,7 @@ namespace WindowsFormsApp1
             this.panel3.Controls.Add(this.listBox1);
             this.panel3.Location = new System.Drawing.Point(153, 30);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(797, 507);
+            this.panel3.Size = new System.Drawing.Size(797, 505);
             this.panel3.TabIndex = 4;
             // 
             // lbMember
@@ -162,7 +162,7 @@ namespace WindowsFormsApp1
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Location = new System.Drawing.Point(0, 30);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(158, 508);
+            this.panel2.Size = new System.Drawing.Size(158, 506);
             this.panel2.TabIndex = 6;
             // 
             // panel4
@@ -170,11 +170,33 @@ namespace WindowsFormsApp1
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel4.Controls.Add(this.label2);
             this.panel4.Controls.Add(this.label1);
-            this.panel4.Location = new System.Drawing.Point(-1, 378);
+            this.panel4.Location = new System.Drawing.Point(0, 378);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(154, 127);
+            this.panel4.Size = new System.Drawing.Size(153, 127);
             this.panel4.TabIndex = 5;
             this.panel4.Click += new System.EventHandler(this.MemberPanel_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("NanumSquare", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(34, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "정   보";
+            this.label2.Click += new System.EventHandler(this.MemberPanel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("NanumSquare", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(34, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "회   원";
+            this.label1.Click += new System.EventHandler(this.MemberPanel_Click);
             // 
             // label6
             // 
@@ -208,12 +230,6 @@ namespace WindowsFormsApp1
             this.label4.Size = new System.Drawing.Size(41, 21);
             this.label4.TabIndex = 1;
             this.label4.Text = "도서";
-            // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 10000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // pictureBox3
             // 
@@ -262,33 +278,17 @@ namespace WindowsFormsApp1
             this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
             this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
             // 
-            // label1
+            // timer1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("NanumSquare", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(34, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 30);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "회   원";
-            this.label1.Click += new System.EventHandler(this.MemberPanel_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("NanumSquare", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(34, 68);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 30);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "정   보";
-            this.label2.Click += new System.EventHandler(this.MemberPanel_Click);
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(950, 537);
+            this.ClientSize = new System.Drawing.Size(950, 535);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
